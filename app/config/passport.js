d813803eeb2582f38700fd14c3c62cbf85f4e61c
C0D1NGD0J0 <web-dev@United4Life.local>
2017-11-17 13:39:31 +0100

    fixed google callback url for prodcution env

--- app/config/passport.js
+++ app/config/passport.js
@@ -65,7 +65,7 @@ passport.use(new GoogleStrategy(
 {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "/auth/google/callback",
     passReqToCallback: true,
     proxy: true
   }, function(request, accessToken, refreshToken, profile, done){
